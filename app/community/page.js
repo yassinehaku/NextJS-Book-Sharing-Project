3040a518122ead6b036df634de14df12fd6e8c17
yassine hakkou <yassine.hakkou.ceo@gmail.com>
2025-07-08 15:58:40 +0100

    community page

--- app/community/page.js
+++ app/community/page.js
@@ -1,3 +1,40 @@
-export default function CommunityPgae() {
-    return <h1> Community Page</h1>;
+import Image from 'next/image';
+
+import bookIcon from '@/assets/icons/book.jpg';
+import communityIcon from '@/assets/icons/community.jpg';
+import eventsIcon from '@/assets/icons/events.jpg';
+import classes from './page.module.css';
+
+export default function CommunityPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          One shared passion: <span className={classes.highlight}>Books</span>
+        </h1>
+        <p>Join our community and share your favorite reads!</p>
+      </header>
+      <main className={classes.main}>
+        <h2>Community Perks</h2>
+
+        <ul className={classes.perks}>
+          <li>
+            <Image src={bookIcon} alt="A stack of books" />
+            <p>Share & discover new books</p>
+          </li>
+          <li>
+            <Image src={communityIcon} alt="A group of people discussing books" />
+            <p>Find fellow book lovers & like-minded readers</p>
+          </li>
+          <li>
+            <Image
+              src={eventsIcon}
+              alt="A group of people at a book event"
+            />
+            <p>Participate in book clubs & author events</p>
+          </li>
+        </ul>
+      </main>
+    </>
+  );
 }
--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,6 +3,7 @@ import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import Image from 'next/image';
 import MainHeaderBackground from './main-header-background';
+import NavLink from './nav-link';
 
 
 export default function MainHeader() {
@@ -18,10 +19,10 @@ export default function MainHeader() {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href="/books">Browse Books</Link>
+            <NavLink href="/books">Browse Books</NavLink>
           </li>
           <li>
-            <Link href="/community">Community</Link>
+            <NavLink href="/community">Community</NavLink>
           </li>
         </ul>
       </nav>
